d31373feefe8f05c93f171e276374b789b6bf872
Pierre Andouche <pierre.andouche@oppida.fr>
2025-05-06 14:52:32 +0200

    feat: support unsupported ordinals

--- command_structures.py
+++ command_structures.py
@@ -10,6 +10,8 @@ Its `body` is either a `_TPM_RQU_BODY` or `_TPM_RSP_BODY`, identified by its `ta
 The `body` of a `_TPM_RQU_BODY` is a `_<cmd>_RQU_BODY`, identified by its `ordinal`.
 
 The `body` of a `_TPM_RSP_BODY` is a `_<cmd>_RSP_BODY`, identified by the `lastCommand` in-scope variable at parsing.
+
+`body` fields are not real fields but are exposed to provide easier access and pretty-printing.
 """
 
 # TPM Main Part 3 TPM Commands
@@ -79,13 +81,32 @@ _TPM_Unseal_RSP_BODY = Struct(
 ######### RQU, RSP, TPM_PACKET(S)
 ###################################################################################################
 
+# hack: global variable + hook to inform of packet size for parsing of unsupported ordinal
+#
+# TODO: remove once 100% ordinal support is implemented
+currentParamSize = None
+
+def param_hook(obj, ctx):
+    global currentParamSize
+
+    # breakpoint()
+
+    # paramSize in a packet is the full size of the packet
+    # we need the remaining number of bytes in the packet, after
+    #     - tag
+    #     - paramSize
+    #     - ordinal or responseCode
+    # n.b. sizeof(TPM_COMMAND_CODE) == sizeof(TPM_RESULT) == 4
+    currentParamSize = ctx._.paramSize - TPM_TAG.sizeof() - Int32ub.sizeof() - TPM_RESULT.sizeof()
+
 _TPM_RQU_BODY = Struct(
-    "ordinal" / TPM_COMMAND_CODE,
+    "ordinal" / TPM_COMMAND_CODE * param_hook,
     "body" / Switch(this.ordinal,
     {
         TPM_COMMAND_CODE.TPM_Unseal: _TPM_Unseal_RQU_BODY,
         # TODO: Everything else
-    }
+    },
+    default=Bytes(lambda _: currentParamSize),
     )
 )
 
@@ -95,12 +116,13 @@ _TPM_RQU_BODY = Struct(
 lastCommand = None
 
 _TPM_RSP_BODY = Struct(
-    "responseCode" / TPM_RESULT,
+    "responseCode" / TPM_RESULT * param_hook,
     "body" / Switch(lambda _: lastCommand, # hack for context-based ordinal inference
     {
         TPM_COMMAND_CODE.TPM_Unseal: _TPM_Unseal_RSP_BODY,
         # TODO: Everything else
-    }
+    },
+    default=Bytes(lambda _: currentParamSize),
     )
 )
 
@@ -124,9 +146,3 @@ def command_hook(obj, ctx):
 TPM_PACKETS = GreedyRange(
     TPM_PACKET * command_hook,
 )
-
-# INTERACTION = Sequence(
-#     TPM_PACKET,
-#     TPM_PACKET,
-# )
-
